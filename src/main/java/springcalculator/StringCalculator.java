ac3e6b3b77f4783a8a3f9795a33dc211404e8693
David Gilligan <java@practice.com>
2020-01-06 20:08:47 +0000

    final Refactoring stage of TDD Practice

--- src/main/java/springcalculator/StringCalculator.java
+++ src/main/java/springcalculator/StringCalculator.java
@@ -3,13 +3,12 @@ package springcalculator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class StringCalculator {
 	
 	String sep = ",";
-	
+	Separator separator;
 	/*
 	 * Method to see if optional separator line exists
 	 * If it exists extract separator and return the numbers
@@ -17,71 +16,22 @@ public class StringCalculator {
 	 * @param string that will be checked if a non-default delimiter is to be used.
 	 * @return returns a String with any newlines removed and just numbers separated by given delimiter.
 	 */
-	private String checkSeperator(String string) {		
-		Pattern regex = Pattern.compile("//(\\[(\\D+)])+\n(.*)");
-		Matcher match = regex.matcher(string);
-		
+	private String checkSeperator(String string) {			 
 		if (string.contains("//")) {
-			String s = "";
-			if(regex.matcher(string).matches()) {
-				StringBuilder sb = new StringBuilder();
-				
-				while(match.find()) {		
-					if(string.contains("][")) {
-						
-						String sepSplit  = "";						
-						String[] separator = match.group(2)
-								.replaceAll("\\]\\[",",")
-								.split(",");
-						StringBuilder multi = new StringBuilder();
-						for(int i = 0; i < separator.length; i++) {							
-							if(i == separator.length - 1) {								
-								sepSplit += "\\" + separator[i];								
-							} else if(separator[i].length() > 1) {								
-								for(int j = 0; j < separator[i].length(); j++) {									
-									multi.append("\\" + separator[i].charAt(j));	
-								}
-								sepSplit += multi.toString() + "|";
-							} else {								
-								sepSplit += "\\" + separator[i] + "|";
-							}							
-						}
-						System.out.println(sepSplit);
-						//create string with numbers and commas
-						String[] numbers = match.group(3).split(sepSplit);
-						
-						for(int i = 0; i < numbers.length;i++) {
-							if (i != numbers.length - 1) {								
-								sb.append(numbers[i] + ",");								
-							} else {								
-								sb.append(numbers[i]);
-							}
-						}
-						
-						//return new string
-						s = sb.toString();
-					} else {
-						
-						int lengthOfDelimitter = string.substring(string.indexOf("[") + 1, string.indexOf("]")).length();
-						String workingString = string.substring(string.indexOf("[") + 1, string.indexOf("]"));
-						
-						for(int i = 0; i < lengthOfDelimitter ;i++) {
-							
-							sb.append("\\" + workingString.charAt(i));
-						}
-						
-						sep = sb.toString();
-						s = string.substring(string.indexOf("\n") + 1);
-					}
-				}			
-				return s;
-			} else {
-				sep = string.substring(2,string.indexOf("\n"));
-				return string.substring(string.indexOf("\n") + 1);
-			}
 			
-		} else {
+			separator = new Separator(string);
+			String returnedString = separator.formatSeparator();
+			
+			if(separator.getSeparator() == null) {				
+				sep = separator.getSeparators();				
+			} else {
+				sep = separator.getSeparator();
+			}			
+			return returnedString;		 
+		} else if (string.contains("\n")) {
 			return string.replaceAll("\n", sep);
+		} else {			
+			return string;
 		}
 	}
 	
@@ -96,10 +46,11 @@ public class StringCalculator {
 			return Integer.parseInt(string);
 		} else if (string.length() > 2) {
 			int sum = 0;
-						
-			String[] numbers = checkSeperator(string).split(sep);		
-			//System.out.println(checkSeperator(string));
+			String createdString = checkSeperator(string);			
+			String[] numbers = createdString.split(sep);
+			
 			for(String number : numbers) {
+				
 				if(Integer.parseInt(number) < 0) {
 					List<Integer> num = new ArrayList<>();
 					num.add(Integer.parseInt(number));
@@ -110,8 +61,9 @@ public class StringCalculator {
 					continue;
 				}
 				number.replaceAll("\n", "");
-				sum += Integer.parseInt(number) ;
-			}			
+				sum += Integer.parseInt(number);
+			}
+			
 			return sum;			
 		} else {
 			return 0;
